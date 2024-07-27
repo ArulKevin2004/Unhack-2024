7add0a2fcf81968ad8c75a440534fb92515f5113
ArulKevin2004 <131542482+ArulKevin2004@users.noreply.github.com>
2024-07-27 16:22:58 +0530

    Milestone-1

---
+++ main.py
@@ -0,0 +1,115 @@
+import pandas as pd
+import math
+
+care_areas = pd.read_csv(r"C:\Users\skava\Desktop\Github\Unhack-2024\resources\Dataset-0(1)\Dataset-0\1st\CareAreas.csv", header=None)
+metadata = pd.read_csv(r"C:\Users\skava\Desktop\Github\Unhack-2024\resources\Dataset-0(1)\Dataset-0\1st\metadata.csv")
+
+main_field_size = metadata.iloc[0,0]
+sub_field_size = metadata.iloc[0,1]
+
+def main_field_calculation(main_field_size, care_areas):
+    # index = 0
+    main_field = []
+    for index,row in care_areas.iterrows():
+        x1 = row[1]
+        x2 = row[2]
+        y1 = row[3]
+        y2 = row[4]
+
+        main_field_x1 = x1
+        main_field_y1 = y1
+        main_field_x2 = main_field_x1 + main_field_size
+        main_field_y2 = main_field_y1 + main_field_size
+        main_field.append([main_field_x1, main_field_x2, main_field_y1, main_field_y2])
+
+    df = pd.DataFrame(main_field)
+    df.to_csv("mainfield.csv",header=False)
+
+
+
+
+def sub_field_calculation(sub_field_size, care_areas):
+    sub_field = []
+    # subid = 0
+    for index,row in care_areas.iterrows():
+
+        x1 = row[1]
+        x2 = row[2]
+        y1 = row[3]
+        y2 = row[4]
+
+        # print("x1 : ",x1,"x2 : ",x2,"y1 : ",y1,"y2 : ",y2)
+
+        x_stage = math.ceil((x2 - x1)/sub_field_size)
+        y_stage = math.ceil((y2 - y1)/sub_field_size)
+
+        # print("No of x field : ",x_stage,"No of y field",y_stage)
+
+        for i in range(y_stage):
+            for j in range(x_stage):
+                sub_field_x1 = x1 + j * sub_field_size
+                sub_field_y1 = y1 + i * sub_field_size
+                sub_field_x2 = sub_field_x1 + sub_field_size
+                sub_field_y2 = sub_field_y1 + sub_field_size
+            
+                if (sub_field_x1 <= x2 and sub_field_y1 <= y2):
+                    sub_field.append([sub_field_x1, sub_field_x2, sub_field_y1, sub_field_y2,index])
+                    # subid = subid+1
+
+
+    df = pd.DataFrame(sub_field)
+
+    df.to_csv("subfield.csv",header=False)
+
+
+
+
+
+        # print("sub_field_x1 : ",sub_field_x1,"sub_field_y1 : ",sub_field_y1,"sub_field_x2 : ",sub_field_x2,"sub_field_y2 : ",sub_field_y2,"Mainfield id: ",index)
+
+    
+
+
+# main_field_calculation(main_field_size, care_areas)
+
+
+main_field_area = pd.read_csv(r"C:\Users\skava\Desktop\Github\Unhack-2024\resources\Dataset-0(1)\Dataset-0\1st\mainfield.csv",header=None)
+
+sub_field_calculation(sub_field_size, care_areas)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
